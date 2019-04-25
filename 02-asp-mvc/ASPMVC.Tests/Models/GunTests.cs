af5c06d87f6b48a647b60385c4072d1faeac66b1
arek-dabrowski <arekdbr97@gmail.com>
2019-04-25 17:09:09 +0200

    test project init

---
+++ 02-asp-mvc/ASPMVC.Tests/Models/GunTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using ASPMVC.Models;
+
+namespace ASPMVC.Tests
+{
+    [TestFixture]
+    public class GunTests
+    {
+        Gun mygun;
+        [SetUp]
+        public void Setup()
+        {
+            mygun = new Gun();
+        }
+
+        [Test]
+        public void ShouldAdd()
+        {
+            //Arrange
+            mygun.Name = "USP";
+
+            //Act
+            String gunName = mygun.Name;
+
+            //Assert
+            Assert.AreEqual("USP", gunName);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            mygun = null;
+        }
+    }
+}
